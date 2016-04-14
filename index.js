bea094f1dd6e88e732fd62753523e0d7f80260bb
Jessica Lord <jlord@github.com>
2016-04-14 09:10:24 -0700

    Update property name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mb = menubar({
   icon: __dirname + '/Icon-Template.png',
   preloadWindow: true,
   width: 420,
-  'window-position': 'topRight'
+  windowPosition: 'topRight'
 })
 const gs = require('electron').globalShortcut
 
